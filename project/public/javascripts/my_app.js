89bcabd57142b414527ca13fcb0d2eae2ef8c41b
Kayla Bixler <kaylabix@comcast.net>
2017-12-12 23:17:24 -0700

    WIP: updating

--- project/public/javascripts/my_app.js
+++ project/public/javascripts/my_app.js
@@ -10,7 +10,6 @@ angular.module('myApp', []).
           console.log(data);
           $scope.user = data;
           $scope.error = "";
-          $scope.highScore = user.high_score;
     })
     .error(function(data, status, headers, config) {
       $scope.user = {};
