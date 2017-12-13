3443ad84310a46553f5cfadbb63fc54a5c7630b7
Kayla Bixler <kaylabix@comcast.net>
2017-12-12 23:01:45 -0700

    High Score -- WIP

--- project/public/javascripts/my_app.js
+++ project/public/javascripts/my_app.js
@@ -3,13 +3,13 @@ angular.module('myApp', []).
   controller('myController', ['$scope', '$http',
                               function($scope, $http) {
 
-    $scope.highScore = localStorage.getItem(jsSnakeHighScore);
+    $scope.highScore = localStorage.getItem("jsSnakeHighScore");
 
     $http.get('/user/profile')
         .success(function(data, status, headers, config) {
+          console.log(data);
           $scope.user = data;
           $scope.error = "";
-          $scope.highScore = "";
     })
     .error(function(data, status, headers, config) {
       $scope.user = {};
